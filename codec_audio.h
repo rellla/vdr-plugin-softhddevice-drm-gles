f7bca8f448f769579f553a20bef2f326deca7e06
derlange2k <44270517+derlange2k@users.noreply.github.com>
2025-05-09 21:08:21 +0200

    Update codec_audio.h

--- codec_audio.h
+++ codec_audio.h
@@ -31,11 +31,12 @@
 //	Defines
 //----------------------------------------------------------------------------
 
-#define CodecPCM  0x01			///< PCM bit mask
-#define CodecMPA  0x02			///< MPA bit mask (planned)
-#define CodecAC3  0x04			///< AC-3 bit mask
-#define CodecEAC3 0x08			///< E-AC-3 bit mask
-#define CodecDTS  0x10			///< DTS bit mask (planned)
+#define CodecPCM  	0x01			///< PCM bit mask
+#define CodecMPA  	0x02			///< MPA bit mask (planned)
+#define CodecAC3  	0x04			///< AC-3 bit mask
+#define CodecEAC3 	0x08			///< E-AC-3 bit mask
+#define CodecDTS  	0x10			///< DTS bit mask (planned)
+#define CodecTrueHD	0x16			///< TrueHD bit mask	
 
 //----------------------------------------------------------------------------
 //	Variables and enums
