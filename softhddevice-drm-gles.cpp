7b8d7a13bf5724228f90cd139f855221bd593fbd
Andreas Baierl <ichgeh@imkreisrum.de>
2025-11-20 21:07:43 +0100

    version 1.2.0

--- softhddevice-drm-gles.cpp
+++ softhddevice-drm-gles.cpp
@@ -57,7 +57,7 @@ extern "C"
 /*****************************************************************************
  * Static variables
  ****************************************************************************/
-static const char *const VERSION = "1.1.2";    ///< vdr-plugin version number
+static const char *const VERSION = "1.2.0";    ///< vdr-plugin version number
                                                ///< Makefile extracts the version number for generating the file name
                                                ///< for the distribution archive.
 
