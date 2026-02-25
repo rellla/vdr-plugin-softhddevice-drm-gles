fabed9e15ee5fa92d660c9821632e41d7bd2bb4c
Andreas Baierl <ichgeh@imkreisrum.de>
2026-02-25 11:21:42 +0100

    h264parser: configure amount of I-Frames to parse

--- videostream.h
+++ videostream.h
@@ -135,7 +135,7 @@ private:
 	int64_t m_inputPts = AV_NOPTS_VALUE;   ///< PTS of the first packet in the input buffer
 	std::vector<std::string> m_naluTypesAtStart;
 	int m_numIFrames = 0;
-	bool m_logPackets = false;
+	int m_logPackets = 0;
 	bool m_dropInvalidPackets = false;
 	std::set<int> m_dpbFrames;
 	int m_maxFrameNum = 1;
