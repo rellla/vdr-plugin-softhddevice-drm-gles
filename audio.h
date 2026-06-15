b3b1b6e34a059caa3367ed5fd915b0877164ce7f
Andreas Baierl <ichgeh@imkreisrum.de>
2026-06-15 14:18:33 +0200

    audio: move alsa to separate class and file

--- audio.h
+++ audio.h
@@ -2,7 +2,7 @@
 
 /**
  * @file audio.h
- * Audio and Alsa Interface Header File
+ * Audio Interface Header File
  *
  * @copyright 2009 - 2014 by Johns.  All Rights Reserved.
  * @copyright 2025 - 2026 by Andreas Baierl. All Rights Reserved.
@@ -11,10 +11,10 @@
  */
 
 /**
- * Audio and Alsa Interface
+ * Audio Interface
  * @defgroup audio Audio Module
  *
- * Handles the audio stream and ALSA interface
+ * Handles the audio stream
  */
 
 #ifndef __AUDIO_H
@@ -23,6 +23,7 @@
 #include <atomic>
 #include <chrono>
 #include <mutex>
+#include <string>
 #include <vector>
 
 extern "C" {
@@ -30,10 +31,9 @@ extern "C" {
 #include <libavfilter/avfilter.h>
 }
 
-#include <alsa/asoundlib.h>
-
 #include <vdr/thread.h>
 
+#include "alsadevice.h"
 #include "audioprocessor.h"
 #include "event.h"
 #include "filllevel.h"
@@ -59,7 +59,7 @@ public:
 	bool IsPaused(void) { return m_paused; };
 	void Filter(AVFrame *, AVCodecContext *);
 	void EnqueueSpdif(const uint16_t *, int, int64_t pts);
-	bool IsBufferFull(void) { return m_pRingbuffer.FreeBytes() <= AUDIO_MIN_BUFFER_FREE; }
+	bool IsBufferFull(void) { return m_pRingbuffer.FreeBytes() <= AUDIO_MIN_BUFFER_FREE; };
 
 	void FlushBuffers(void);
 	int GetUsedRingbufferBytes(void);
@@ -67,24 +67,24 @@ public:
 	int64_t GetHardwareOutputPtsMs(void);
 	int64_t GetHardwareOutputDelayMs(void);
 	int64_t GetHardwareOutputPtsTimebaseUnits(void);
-	int GetPassthroughMask(void) const { return m_passthroughMask; }
-	bool HasInputPts(void) { return m_inputPts != AV_NOPTS_VALUE; }
-	int64_t GetInputPtsMs(void) { return PtsToMs(m_inputPts); }
+	bool HasInputPts(void) { return m_inputPts != AV_NOPTS_VALUE; };
+	int64_t GetInputPtsMs(void) { return m_alsa.PtsToMs(m_inputPts, av_q2d(m_timebase)); };
 	int64_t GetOutputPtsMs(void);
 	int GetAvResyncBorderMs(void) { return AV_SYNC_BORDER_MS; };
 
 	void SetVolume(int);
-	void SetDownmix(int downMix) { m_downmix = downMix; };
 	void SetSoftvol(bool softVolume) { m_softVolume = softVolume; };
 
 	void SetNormalize(bool, int);
 	void SetCompression(bool, int);
 	void SetEqualizer(bool, int[18]);
-
 	void SetStereoDescent(int);
-	void SetPassthroughMask(int);
-	void SetAutoAES(bool appendAes) { m_appendAES = appendAes; }
-	void SetTimebase(AVRational timebase) { m_pTimebase = timebase; };
+
+	void SetPassthroughMask(int mask) { m_alsa.SetPassthroughMask(mask); };
+	void SetAutoAES(bool appendAes) { m_alsa.SetAutoAES(appendAes); };
+	void SetTimebase(AVRational timebase) { m_timebase = timebase; };
+	void SetDownmix(int downMix) { m_alsa.SetDownmix(downMix); };
+	int GetPassthroughMask(void) const { return m_alsa.GetPassthroughMask(); };
 
 	void DropSamplesOlderThanPtsMs(int64_t);
 	void ClockDriftCompensation(void);
@@ -101,18 +101,15 @@ private:
 
 	cSoftHdDevice *m_pDevice;               ///< pointer to device
 	cSoftHdConfig *m_pConfig;               ///< pointer to config
+	cAlsaDevice m_alsa;                     ///< alsa device
 	IEventReceiver *m_pEventReceiver;       ///< pointer to event receiver
 	cBufferFillLevelLowPassFilter m_fillLevel;                  ///< low pass filter for the buffer fill level
 	cPidController m_pidController{3, 0.005, 0, 1000};          ///< PID controller for clock drift compensation with tuning values coming from educated guesses
 	std::chrono::steady_clock::time_point m_lastPidInvocation;  ///< last time the PID controller was invoked
-	snd_pcm_uframes_t m_alsaBufferSizeFrames = 0; ///< alsa buffer size in frames
 	int m_packetCounter = 0;                ///< packet counter for logging
 
-	// common audio, alsa
+	// common audio
 	bool m_initialized = false;             ///< class initialized
-	unsigned int m_hwSampleRate = 0;        ///< hardware sample rate in Hz
-	unsigned int m_hwNumChannels = 0;       ///< number of hardware channels
-	AVRational m_pTimebase;                 ///< pointer to AVCodecContext pkts_timebase
 	std::mutex m_mutex;                     ///< mutex for thread safety
 	std::mutex m_pauseMutex;                ///< mutex for a safe thread pausing
 	std::vector<Event> m_eventQueue;        ///< event queue for incoming events
@@ -121,19 +118,15 @@ private:
 
 	int m_volume = 0;                       ///< current volume (0 .. 1000)
 	int m_stereoDescent;                    ///< volume descent for stereo
-	int m_downmix;                          ///< set stereo downmix
-	std::atomic<bool> m_passthroughActive = false; ///< set, if passthrough is active
+	AVRational m_timebase;                  ///< AVCodecContext pkts_timebase
 
 	int64_t m_inputPts = AV_NOPTS_VALUE;    ///< pts clock (last pts in ringbuffer)
 	std::atomic<bool> m_paused = true;      ///< audio is paused
 
 	bool m_softVolume;                      ///< flag to use soft volume
-	int m_passthroughMask;                  ///< passthrough mask
-	const char *m_pAlsaPCMDevice;           ///< Alsa PCM device name
-	bool m_appendAES;                       ///< flag ato utomatic append AES
 	int m_spdifBurstSize = 0;               ///< size of the current spdif burst
 	std::vector<uint16_t> m_pauseBurst;     ///< holds the burst data itself
-	snd_pcm_sframes_t m_hwBaseline = 0;     ///< saves the hw delay (pause bursts) once a real audio frame to correctly do the AV-Sync
+	int m_hwBaseline = 0;                   ///< saves the hw delay (pause bursts) once a real audio frame to correctly do the AV-Sync
 	bool m_firstRealAudioReceived = false;  ///< false, as long as no real audio was sent - used to trigger the baseline set
 
 	void Enqueue(const uint16_t *, int, int64_t);
@@ -142,6 +135,10 @@ private:
 	bool SendPause(void);
 	void BuildPauseBurst(void);
 	void Stop(void);
+	void FlushAlsaBuffers(void);
+	void DropAlsaBuffers(void);
+	bool CyclicCall(void);
+	void ProcessEvents(void);
 
 	// audio manipulation
 	cAudioProcessor m_audioProcessor;
@@ -162,40 +159,13 @@ private:
 	int InitFilter(AVCodecContext *);
 	AVFrame *FilterGetFrame(void);
 	int CheckForFilterReady(AVCodecContext *);
+	std::string BuildChannelMapFilter(const AVChannelLayout &);
 
 	// ring buffer variables
 	static constexpr unsigned RINGBUFFER_SIZE = 3 * 5 * 7 * 8 * 2 * 1000; ///< default ring buffer size ~2s 8ch 16bit (3 * 5 * 7 * 8)
 	cSoftHdRingbuffer m_pRingbuffer{RINGBUFFER_SIZE};                     ///< sample ring buffer
 
-	// alsa
-	snd_pcm_t *m_pAlsaPCMHandle;         ///< alsa pcm handle
-	snd_mixer_t *m_pAlsaMixer = nullptr; ///< alsa mixer handle
-	snd_mixer_elem_t *m_pAlsaMixerElem = nullptr; ///< alsa mixer element
-	int m_alsaRatio;                     ///< internal -> mixer ratio * 1000
-	bool m_alsaUseMmap;                  ///< use mmap
-
-	int AlsaSetup(int, int, bool);
-	char *OpenAlsaDevice(const char *);
-	char *FindAlsaDevice(const char *, const char *);
-	void AlsaInitDevice(void);
-	void AlsaInitMixer(void);
-	void AlsaSetVolume(int);
-	void AlsaInit(void);
-	void AlsaExit(void);
-	void FlushAlsaBuffers(void);
-	void DropAlsaBuffers(void);
-	void FlushAlsaBuffersInternal(bool);
-	bool CyclicCall(void);
-	void ProcessEvents(void);
-	void HandleError(int);
-
 	int64_t GetOutputPtsMsInternal(void);
-	int64_t PtsToMs(int64_t pts) { return pts * av_q2d(m_pTimebase) * 1000; }
-	int64_t MsToPts(int64_t ptsMs) { return ptsMs / av_q2d(m_pTimebase) / 1000; }
-	int MsToFrames(int milliseconds) { return (int64_t)milliseconds * m_hwSampleRate / 1000; }
-	int FramesToMs(int frames) { return (int64_t)frames * 1000 / m_hwSampleRate; }
-	int64_t FramesToPts(int frames) { return MsToPts((int64_t)frames * 1000 / m_hwSampleRate); }
-	double FramesToMsDouble(int frames) { return (double)frames * 1000 / m_hwSampleRate; }
 };
 
 #endif
